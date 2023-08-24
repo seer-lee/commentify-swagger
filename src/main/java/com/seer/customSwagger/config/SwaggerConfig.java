7307fe0ad6a364fbf6cc27560684a5085c38ed55
seer-lee <2seer@naver.com>
2023-08-24 18:52:10 +0900

    delete: slf4j 어노테이션 삭제

--- src/main/java/com/seer/customSwagger/config/SwaggerConfig.java
+++ src/main/java/com/seer/customSwagger/config/SwaggerConfig.java
@@ -2,13 +2,10 @@ package com.seer.customSwagger.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Slf4j
 public class SwaggerConfig {
     @Bean
     public ModelResolver modelResolver(ObjectMapper objectMapper) {
